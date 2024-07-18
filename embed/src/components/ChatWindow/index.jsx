5d32d1d786a7a96a97338fe251b109aeeb343822
tonghe <tonghe@emigreat.co>
2024-07-18 14:41:40 +0200

    remove reset chat link; remove session id button; changed chat button icon

--- embed/src/components/ChatWindow/index.jsx
+++ embed/src/components/ChatWindow/index.jsx
@@ -4,7 +4,6 @@ import useChatHistory from "@/hooks/chat/useChatHistory";
 import ChatContainer from "./ChatContainer";
 import Sponsor from "../Sponsor";
 import { ChatHistoryLoading } from "./ChatContainer/ChatHistory";
-import ResetChat from "../ResetChat";
 
 export default function ChatWindow({ closeChat, settings, sessionId }) {
   const { chatHistory, setChatHistory, loading } = useChatHistory(
@@ -50,11 +49,6 @@ export default function ChatWindow({ closeChat, settings, sessionId }) {
         />
       </div>
       <div className="allm-mt-4 allm-pb-4 allm-h-fit allm-gap-y-2 allm-z-10">
-      <ResetChat
-          setChatHistory={setChatHistory}
-          settings={settings}
-          sessionId={sessionId}
-        />
         <Sponsor settings={settings} />
       </div>
     </div>
